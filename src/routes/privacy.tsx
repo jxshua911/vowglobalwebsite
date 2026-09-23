2e049d89bbf08a6082a6d40769cec19d006c4b00
jxshua911 <joshuakasanga9@gmail.com>
2026-09-24 00:20:21 +0300

    Remove redundant VOW page; content is consolidated into Legal

--- src/routes/privacy.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-// Alias: /privacy permanently redirects to the canonical /privacy-policy URL.
-export const Route = createFileRoute("/privacy")({
-  beforeLoad: () => {
-    throw redirect({ to: "/privacy-policy", statusCode: 301 });
-  },
-});
