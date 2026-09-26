7f72a7bfcfa6d4b73b7b4ea3d9f56aa8d1f7705c
jxshua911 <joshuakasanga9@gmail.com>
2026-09-26 21:53:06 +0300

    remove: retire how-it-works route

--- src/routes/how-it-works.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/how-it-works")({
-  beforeLoad: () => { throw redirect({ to: "/about-vow" }); },
-});
